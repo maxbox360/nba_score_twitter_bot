3f2c98937703a7a712bc53dfb03ba7d850c98f2c
Max Sanderlin <msanderlin08@gmail.com>
2023-12-29 16:09:21 -0600

    Fix Twitter Authentication :)

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ def get_env(name):
 
 
 if __name__ == "__main__":
-    api_key = get_env('TWITTER_API_KEY')
-    api_secret_key = get_env('TWITTER_API_SECRET')
-    access_token = get_env('TWITTER_ACCESS_TOKEN')
-    access_token_secret = get_env('TWITTER_TOKEN_SECRET')
+    consumer_key = get_env('CONSUMER_KEY')
+    consumer_secret = get_env('CONSUMER_SECRET')
+    access_token = get_env('ACCESS_TOKEN')
+    access_token_secret = get_env('TOKEN_SECRET')
 
-    nba = NBA(api_key=api_key, api_secret_key=api_secret_key, access_token=access_token, access_token_secret=access_token_secret)
+    nba = NBA(consumer_key=consumer_key, consumer_secret=consumer_secret,
+              access_token=access_token, token_secret=access_token_secret)
     nba.main()
--- nba.py
+++ nba.py
@@ -1,17 +1,64 @@
-import tweepy
+from requests_oauthlib import OAuth1Session
 import pandas as pd
 import requests
 pd.set_option('display.max_columns', None)
 import os
+import json
 
 
 # noinspection PyMethodMayBeStatic
 class NBA:
 
-    def __init__(self, api_key, api_secret_key, access_token, access_token_secret):
+    def __init__(self, consumer_key, consumer_secret, access_token, token_secret):
         # User-based authentication (API keys and access tokens)
-        self.user_api = tweepy.Client(consumer_key=api_key, consumer_secret=api_secret_key,
-                             access_token=access_token, access_token_secret=access_token_secret)
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_token = access_token
+        self.token_secret = token_secret
+
+        # Get request token
+        request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+        oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+
+        try:
+            fetch_response = oauth.fetch_request_token(request_token_url)
+        except ValueError:
+            print(
+                "There may have been an issue with the consumer_key or consumer_secret you entered."
+            )
+
+        resource_owner_key = fetch_response.get("oauth_token")
+        resource_owner_secret = fetch_response.get("oauth_token_secret")
+        print("Got OAuth token: %s" % resource_owner_key)
+
+        # Get authorization
+        base_authorization_url = "https://api.twitter.com/oauth/authorize"
+        authorization_url = oauth.authorization_url(base_authorization_url)
+        print("Please go here and authorize: %s" % authorization_url)
+        verifier = input("Paste the PIN here: ")
+
+        # Get the access token
+        access_token_url = "https://api.twitter.com/oauth/access_token"
+        oauth = OAuth1Session(
+            consumer_key,
+            client_secret=consumer_secret,
+            resource_owner_key=resource_owner_key,
+            resource_owner_secret=resource_owner_secret,
+            verifier=verifier,
+        )
+        oauth_tokens = oauth.fetch_access_token(access_token_url)
+
+        access_token = oauth_tokens["oauth_token"]
+        access_token_secret = oauth_tokens["oauth_token_secret"]
+
+        # Make the request
+        self.oauth = OAuth1Session(
+            consumer_key,
+            client_secret=consumer_secret,
+            resource_owner_key=access_token,
+            resource_owner_secret=access_token_secret,
+        )
+
 
     def get_ordinal_suffix(self, number):
         if 10 <= number % 100 <= 20:
@@ -62,36 +109,50 @@ class NBA:
                 passed_players_names = passed_players['PLAYER_NAME'].tolist()
 
                 ordinal_suffix = self.get_ordinal_suffix(current_rank)
-                try:
-                    tweet = f"{player_name} has become the {current_rank}{ordinal_suffix} ranked scorer all time in NBA history. " \
-                        f"He has scored {points:,} points in his career, passing: {', '.join(passed_players_names)}"
+                tweet = f"{player_name} has become the {current_rank}{ordinal_suffix} ranked scorer all time in NBA history. " \
+                    f"He has scored {points:,} points in his career, passing: {', '.join(passed_players_names)}"
 
-                    self.user_api.create_tweet(text=tweet)
-                except tweepy.TweepyException as e:
-                    print(f"Error posting tweet: {e}")
+                payload = {"text": tweet}
+
+                # Making the request
+                response = self.oauth.post(
+                    "https://api.twitter.com/2/tweets",
+                    json=payload,
+                )
+
+                if response.status_code != 201:
+                    raise Exception(
+                        "Request returned an error: {} {}".format(response.status_code, response.text)
+                    )
+
+                print("Response code: {}".format(response.status_code))
+
+                # Saving the response as JSON
+                json_response = response.json()
+                print(json.dumps(json_response, indent=4, sort_keys=True))
 
 
     def main(self):
         """Keeping this commented out code for debugging purposes"""
-        # Example of current week's table (replace this with your actual data)
-        current_data = {
-            'PLAYER_ID': [1, 5, 2, 3, 4],
-            'PLAYER_NAME': ['LeBron James', 'Dirk Nowitzki', 'Karl Malone', 'Kobe Bryant', 'Michael Jordan'],
-            'PTS_RANK': [1, 2, 3, 4, 5],
-            'PTS': [35291, 33644, 33643, 32292, 31419]
-        }
-
-        current_table = pd.DataFrame(current_data)
-
-        # Example of previous week's table (replace this with your actual data)
-        previous_data = {
-            'PLAYER_ID': [1, 2, 3, 4, 5],
-            'PLAYER_NAME': ['LeBron James', 'Karl Malone', 'Kobe Bryant', 'Michael Jordan', 'Dirk Nowitzki'],
-            'PTS_RANK': [1, 2, 3, 4, 5],
-            'PTS': [36928, 33643, 32292, 31419, 31129]
-        }
-
-        previous_table = pd.DataFrame(previous_data)
+        # # Example of current week's table (replace this with your actual data)
+        # current_data = {
+        #     'PLAYER_ID': [1, 5, 2, 3, 4],
+        #     'PLAYER_NAME': ['LeBron James', 'Dirk Nowitzki', 'Karl Malone', 'Kobe Bryant', 'Michael Jordan'],
+        #     'PTS_RANK': [1, 2, 3, 4, 5],
+        #     'PTS': [35291, 33644, 33643, 32292, 31419]
+        # }
+        #
+        # current_table = pd.DataFrame(current_data)
+        #
+        # # Example of previous week's table (replace this with your actual data)
+        # previous_data = {
+        #     'PLAYER_ID': [1, 2, 3, 4, 5],
+        #     'PLAYER_NAME': ['LeBron James', 'Karl Malone', 'Kobe Bryant', 'Michael Jordan', 'Dirk Nowitzki'],
+        #     'PTS_RANK': [1, 2, 3, 4, 5],
+        #     'PTS': [36928, 33643, 32292, 31419, 31129]
+        # }
+        #
+        # previous_table = pd.DataFrame(previous_data)
 
         website = 'https://stats.nba.com'
         stats = 'PTS'
@@ -100,17 +161,17 @@ class NBA:
 
         url = self.customize_nba_url(website=website, stats=stats, season_type=season_type, season=season)
 
-        # # Get the file names
-        # current_week_filename = 'nba_all_time_scoring_current_week.csv'
-        # previous_week_filename = 'nba_all_time_scoring_previous_week.csv'
-        #
-        # # Create the new table and grab the previous week table
-        # current_table = self.fetch_nba_all_time_scoring(url)
-        # previous_table = self.load_table_from_file(previous_week_filename)
-        #
-        # # Save the new data to the current_week file
-        # self.save_table_to_file(current_table, current_week_filename)
+        # Get the file names
+        current_week_filename = 'nba_all_time_scoring_current_week.csv'
+        previous_week_filename = 'nba_all_time_scoring_previous_week.csv'
+
+        # Create the new table and grab the previous week table
+        current_table = self.fetch_nba_all_time_scoring(url)
+        previous_table = self.load_table_from_file(previous_week_filename)
+
+        # Save the new data to the current_week file
+        self.save_table_to_file(current_table, current_week_filename)
         self.compare_tables(current_table, previous_table)
 
-        # # Save this week's table to previous file so we can compare next week
-        # self.save_table_to_file(current_table, previous_week_filename)
+        # Save this week's table to previous file so we can compare next week
+        self.save_table_to_file(current_table, previous_week_filename)
