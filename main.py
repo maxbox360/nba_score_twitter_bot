94c7a1b7c1292a4f2bb8cfa9d4413f7162c58a80
Max Sanderlin <msanderlin08@gmail.com>
2023-12-30 11:15:53 -0600

    Removing unneeded secrets

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ def get_env(name):
 if __name__ == "__main__":
     consumer_key = get_env('CONSUMER_KEY')
     consumer_secret = get_env('CONSUMER_SECRET')
-    access_token = get_env('ACCESS_TOKEN')
-    access_token_secret = get_env('TOKEN_SECRET')
 
     nba = NBA(consumer_key=consumer_key, consumer_secret=consumer_secret)
     nba.main()
